3668691e351c4789bad802582245472554b5ca99
Mohsen Farzadipour <onemohsen@gmail.com>
2022-05-19 11:38:54 +0430

    feat: impliment credit package seeder and factory

---
+++ database/seeders/CreditPackegeSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Database\Seeders;
+
+use App\Models\CreditPackage;
+use Illuminate\Database\Seeder;
+
+class CreditPackegeSeeder extends Seeder
+{
+    public function run(): void
+    {
+        CreditPackage::factory(5)->create();
+    }
+}
