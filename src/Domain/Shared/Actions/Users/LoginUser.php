948e3968e238c66566380fbbc219d923265d99fc
Mohsen Farzadipour <onemohsen@gmail.com>
2022-05-27 20:55:21 +0430

    fix: request send when server run with artisan

--- src/Domain/Shared/Actions/Users/LoginUser.php
+++ src/Domain/Shared/Actions/Users/LoginUser.php
@@ -5,28 +5,22 @@ declare(strict_types=1);
 namespace Domain\Shared\Actions\Users;
 
 use Domain\Shared\ValueObjects\UserValueObject;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
 
 class LoginUser
 {
     public static function handle(UserValueObject $object): array
     {
-        try {
-            $response = Http::post(route('api.v1.passport.token'), [
-                'grant_type' => config('snapppay.api.grant_type'),
-                'client_id' => config('snapppay.api.client_id'),
-                'client_secret' => config('snapppay.api.client_secret'),
-                'username' => $object->email,
-                'password' => $object->password,
-            ]);
-        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
-            if ($e->getCode() == 400) abort(Response::HTTP_UNAUTHORIZED, 'Invalid Request');
-            if ($e->getCode() == 401) abort(Response::HTTP_UNAUTHORIZED, 'Invalid Credentials');
-        } catch (\Exception $e) {
-            abort(Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        $req =  Request::create(route('api.v1.passport.token'), 'post', [
+            'grant_type' => config('snapppay.api.grant_type'),
+            'client_id' => config('snapppay.api.client_id'),
+            'client_secret' => config('snapppay.api.client_secret'),
+            'username' => $object->email,
+            'password' => $object->password,
+        ]);
 
-        return $response->json();
+        $response = app()->handle($req);
+
+        return json_decode($response->getContent(), true);
     }
 }
